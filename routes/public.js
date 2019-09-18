31c84fb449f59c34420a36b7d0335ff998a11f8e
Andrew Markin <andrew.n.markin@gmail.com>
2019-09-19 01:34:50 +0300

    Project initialized

---
+++ routes/public.js
@@ -0,0 +1,8 @@
+const express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+  res.json({ success: true, message: "Welcome to the Party Picker!" });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+
+var server = express();
+server.use(morgan('dev'));
+
+server.use('/', require('./routes/public'));
+
+// Catch 404 and forward to error handler
+server.use(function(_req, _res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+server.use(function(err, _req, res, _next) {
+  console.error('Error:', err);
+  res.status(err.status || 500);
+  res.json({
+    message: err.message
+  });
+});
+
+server.listen(process.env.PORT || 3000, function(err) {
+  if (err) {
+    console.error("Unable to start server", err.message);
+    process.exit(2);
+    return;
+  }
+  console.log('Server started...');
+});
+
+process.on('SIGINT', function() {
+  console.log( "\nShutting down..." );
+  process.exit(0);
+});
