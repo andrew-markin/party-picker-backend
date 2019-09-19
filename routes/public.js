62e0039c99e923aafb69a7ee6705bb31ddbc169b
Andrew Markin <andrew.n.markin@gmail.com>
2019-09-19 22:28:39 +0300

    Minor fixes

--- routes/public.js
+++ routes/public.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 var router = express.Router();
 
-router.get("/", function(req, res, next) {
-  res.json({ success: true, message: "Welcome to the Party Picker!" });
+router.get('/', function(req, res, next) {
+  res.json({ success: true, message: 'Welcome to the Party Picker!' });
 });
 
 module.exports = router;
