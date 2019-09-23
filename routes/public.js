c1c887fc80d126510fdc0c53281ca12531e65ba8
Andrew Markin <andrew.n.markin@gmail.com>
2019-09-23 21:39:52 +0300

    Requests validation implemented

--- routes/public.js
+++ routes/public.js
@@ -1,10 +1,24 @@
 const express = require('express');
+
 const users = require('../services/users');
+const { validate, body } = require('../utilities/validator');
 
 var router = express.Router();
 
+// Common validation checks
+
+const checkUsername = body('username').exists().isString().trim()
+  .isLength({ min: 5, max: 20 }).withMessage('Username must be 5-20 characters long')
+  .matches(/^[a-zA-Z0-9\-]*$/).withMessage('Username can include only: a-z, A-Z, 0-9 and \'-\'');
+
+const checkPassword = body('password').exists().isString().trim()
+  .isLength({ min: 8, max: 32 }).withMessage('Password must be 8-32 characters long')
+  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_@$!%*?&])[A-Za-z\d_@$!%*?&]*$/)
+  .withMessage('Password must include one lowercase letter, one uppercase letter, ' +
+               'a number, and a special character (_@$!%*?&)');
+
 // User registration
-router.post('/users', function(req, res, next) {
+router.post('/users', validate([checkUsername, checkPassword]), function(req, res, next) {
   users.register({
     username: req.body.username,
     password: req.body.password
@@ -24,7 +38,7 @@ router.post('/users', function(req, res, next) {
 });
 
 // User authentication
-router.post('/users/auth', function(req, res, next) {
+router.post('/users/auth', validate([checkUsername, checkPassword]), function(req, res, next) {
   users.authenticate({
     username: req.body.username,
     password: req.body.password
